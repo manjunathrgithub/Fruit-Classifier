b111768d4c58fb0ef77aa74ea831c2c7f8f1d139
manjunathrgithub <56949759+manjunathrgithub@users.noreply.github.com>
2019-10-30 16:56:56 -0700

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -14,7 +14,7 @@ function showPicked(input) {
 
 function analyze() {
     var uploadFiles = el('file-input').files;
-    if (uploadFiles.length != 1) alert('Please select 1 file to analyze!');
+    if (uploadFiles.length != 1) alert('Please select 1 fruit image to analyze!');
 
     el('analyze-button').innerHTML = 'Analyzing...';
     var xhr = new XMLHttpRequest();
